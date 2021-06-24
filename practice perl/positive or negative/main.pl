015a112e4925ce53ea532f69737f99705a2b84a8
bruhmoment3124 <73769756+bruhmoment3124@users.noreply.github.com>
2021-06-24 14:27:48 -0400

    Add files via upload

--- practice perl/positive or negative/main.pl
+++ practice perl/positive or negative/main.pl
@@ -3,9 +3,9 @@ print "enter a number:\n";
 $number = <>;
 
 if($number < 0) { # less than 0
-		print "$number is negative\n";
+	print "$number is negative\n";
 } elsif($number > 0) { # greater than zero
-		print "$number is positive\n";
+	print "$number is positive\n";
 } elsif($number == 0) { # zero
-		print "$number equals zero";
+	print "$number equals zero";
 }
