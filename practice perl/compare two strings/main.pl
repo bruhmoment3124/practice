194a51aed24c768cded27c88b30064866c3fb2a3
bruhmoment3124 <73769756+bruhmoment3124@users.noreply.github.com>
2021-06-30 10:57:23 -0400

    Add files via upload

---
+++ practice perl/compare two strings/main.pl
@@ -0,0 +1,13 @@
+
+print "enter string 1:\n";
+$str1 = <>;
+print "enter string 2:\n";
+$str2 = <>;
+
+$ret = $str1 cmp $str2; # compare the 2 strings
+
+if($ret == 1) {
+		print "the strings are different\n"
+} elsif($ret == 0) {
+		print "the strings are the same\n";
+}
