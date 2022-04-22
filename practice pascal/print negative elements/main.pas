58a0ea504d38f15d532c9d822d5e2b52a9d21c4b
bruhmoment3124 <73769756+bruhmoment3124@users.noreply.github.com>
2022-04-22 18:36:31 -0400

    Add files via upload

---
+++ practice pascal/print negative elements/main.pas
@@ -0,0 +1,16 @@
+program main;
+
+var
+	i, k: integer;
+	arr: array [1..5] of integer;
+
+begin
+	for i := -3 to 1 do
+	begin
+		arr[i] := i; (* assign numbers to array -3 to 1 *)
+ 	
+		if arr[i] < 0 then (* only print if they are negative *)
+			writeln(arr[i]);
+	end;
+	readln();
+end.
